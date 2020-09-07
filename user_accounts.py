481a4ce34dd120f4b6c850832f9478344551ed2b
kiira254 <nkamotho57@gmail.com>
2020-09-07 11:22:19 +0300

    add user accouunt method

--- user_accounts.py
+++ user_accounts.py
@@ -25,3 +25,34 @@ class userAccounts:
 
     def random_password(length, chars=string.ascii_letters + string.digits):
         return ''.join(random.choice(chars) for _ in range(length))
+  @classmethod
+    def account_exist(cls, platform, username):
+        for account in cls.accounts:
+            if account.username == username and account.platform == platform:
+                return True
+            return False
+
+    @classmethod
+    def get_account(cls, platform, username):
+        '''
+        this function searchs for an existing account relative to its username and the platform given
+        '''
+        for account in cls.accounts:
+            if account.username == username and account.platform == platform:
+                return account
+
+    @classmethod
+    def list_all_credentials(cls):
+        '''
+        function that allows users to view their account details
+        '''
+        i=0
+        for account in cls.accounts:
+            i+=1
+            print(f'  =>{i}. Platform {account.platform} Username: {account.username} password: {account.password}')
+
+    @classmethod
+    def copy_password(cls, username, platform):
+        for account in cls.accounts:
+            if account.username == username and account.platform == platform:
+                return pyperclip.copy(account.password)
