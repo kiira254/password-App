0f8cae6b678609be32d5c0839c959cd5ba942695
kiira254 <nkamotho57@gmail.com>
2020-09-07 11:21:49 +0300

    add user accouunt def

--- user_accounts.py
+++ user_accounts.py
@@ -7,3 +7,21 @@ class userAccounts:
     Class that stores user accounts and credentials
     '''
     accounts = []
+
+    def __init__(self, platform, username, password):
+        '''
+        this function allows users to store their various account details
+            Args: platform,username,password
+        '''
+        self.platform = platform
+        self.username = username
+        self.password = password
+
+    def add_account(self):
+        '''
+        this function adds accounts to the accounts list
+        '''
+        self.accounts.append(self)
+
+    def random_password(length, chars=string.ascii_letters + string.digits):
+        return ''.join(random.choice(chars) for _ in range(length))
