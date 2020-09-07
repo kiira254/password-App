f0dc1a33ae192feec45aa4fe5cd7bb6be66599ea
kiira254 <nkamotho57@gmail.com>
2020-09-07 12:59:13 +0300

    add user def

--- user.py
+++ user.py
@@ -3,3 +3,16 @@ class User:
     Class that allows the users to create their login details
     '''
     user_info = []
+    def __init__(self, first_name, last_name, username, password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.password = password
+
+    def save_user_info(self):
+        '''
+        function that appends user login info
+        '''
+        self.user_info.append(self)
+
+    
