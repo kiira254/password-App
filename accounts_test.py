871254d365b0aeb1b207bd4470c0dbeeb970a9ec
kiira254 <nkamotho57@gmail.com>
2020-09-07 11:26:24 +0300

    add user accouunt test imports

--- accounts_test.py
+++ accounts_test.py
@@ -1,2 +1,7 @@
 import unittest
 from user_accounts import userAccounts
+
+class AccountsTest(unittest.TestCase):
+    '''
+    Class that performs testing of the various functions of accounts.py
+    '''
