0f0f864bcf7cca72e6d9243272e940e7ecda6f6e
kiira254 <nkamotho57@gmail.com>
2020-09-07 12:59:59 +0300

    add user methods

--- user.py
+++ user.py
@@ -15,4 +15,32 @@ class User:
         '''
         self.user_info.append(self)
 
+    @classmethod
+    def user_exists(cls,username):
+        '''
+        a class function that checks if a certain user with the inputted username exists and returns true or false
+        '''
+        for user in cls.user_info:
+            if user.username==username:
+                return True
+        return False
+
+    @classmethod
+    def get_user_info(cls,username):
+        '''
+        function that allows users to view their account details
+        '''
+        for user in cls.user_info:
+            if user.username == username:
+                return user
+    
+    @classmethod
+    def user_login(cls,username,password):
+        '''
+        function that checks if the username of a registered account equals the inputted password
+        '''
+        if cls.get_user_info(username).password == password:
+            return True
+        return False
+
     
