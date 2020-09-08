e32c01ca0bbc6cc7e3ac3a74bf2b415c3e579d4c
kiira254 <nkamotho57@gmail.com>
2020-09-08 18:40:22 +0300

    add

--- accounts_test.py
+++ accounts_test.py
@@ -6,14 +6,14 @@ class AccountsTest(unittest.TestCase):
     Class that performs testing of the various functions of accounts.py
     '''
     def setUp(self):
-        self.new_account = userAccounts('instagram', 'cephaske254', 'admin')
+        self.new_account = userAccounts('instagram', 'kiira254', 'admin')
 
     def tearDown(self):
         userAccounts.accounts = []
 
     def test_init(self):
         self.assertEqual(self.new_account.platform, 'instagram')
-        self.assertEqual(self.new_account.username, 'cephaske254')
+        self.assertEqual(self.new_account.username, 'kiira254')
         self.assertEqual(self.new_account.password, 'admin')
 
     def test_add_account(self):
@@ -28,15 +28,15 @@ class AccountsTest(unittest.TestCase):
         test case that checks if an account is already registered. If it is registered, it returns the account
         '''
         userAccounts.add_account(self.new_account)
-        self.assertTrue(userAccounts.account_exist('instagram', 'cephaske254'))
+        self.assertTrue(userAccounts.account_exist('instagram', 'kiira254'))
 
     def test_get_account(self):
         '''
         test case that checks if an account is already registered. If it is registered, it returns the account
         '''
         userAccounts.add_account(self.new_account)
-        get_account = userAccounts.get_account('instagram', 'cephaske254')
-        self.assertEqual(get_account.username,'cephaske254')
+        get_account = userAccounts.get_account('instagram', 'kiira254')
+        self.assertEqual(get_account.username,'kiira254')
         self.assertEqual(get_account.email,'test@mail.com')
 
 
